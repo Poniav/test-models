10f77b36bdf8c12cbdb59b1b01a770a4d004be61
Guillaume G <guigarciafr@gmail.com>
2025-01-26 22:57:39 +0100

    Edit path config & server

--- app/config.py
+++ app/config.py
@@ -1,3 +1,8 @@
+"""
+Config file for the application
+Init the basic settings 
+"""
+
 from dotenv import load_dotenv
 import os
 
@@ -5,6 +10,10 @@ class Config:
 
     load_dotenv()
 
+    # Load the environment variables
     PUB_KEY = os.getenv('PUB_KEY')
     PRIV_KEY = os.getenv('PRIV_KEY')
     DB_URI = os.getenv('DB_URI')
+
+    # Path to the resources folder
+    RESOURCES_PATH = os.path.abspath("resources")
