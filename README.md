c9bbb920b81d29f41043db7fdf09d63f82883138
Guillaume G <guigarciafr@gmail.com>
2025-01-30 09:13:12 +0100

    Add details & output folder

---
+++ README.md
@@ -0,0 +1,4 @@
+# App & AI Loading Models
+
+1. stableDiffusion1-0 (SDXL1_0_Refined)
+2. Helsinki Autoload (Helsinki_al)
--- app/model_stableDiffusion1-0.py
+++ app/model_stableDiffusion1-0.py
@@ -12,6 +12,9 @@ from diffusers import StableDiffusionXLPipeline, StableDiffusionXLImg2ImgPipelin
 import torch
 from PIL import Image
 
+# Path output: output/output_base_SDXL1_0.png
+path = "output/"
+
 # Check if CUDA is available
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -26,10 +29,11 @@ refiner_pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(
 ).to(device)
 
 # Parameters for generation
-prompt = "A futuristic cityscape at sunset, ultra-realistic, vibrant colors"
-negative_prompt = "low quality, blurry, unrealistic"
-guidance_scale = 7.5
-num_inference_steps = 50
+#prompt = "A futuristic cityscape at sunset, ultra-realistic, vibrant colors"
+prompt = "A hyper-realistic portrait of a beautiful ginger girl with freckles, detailed skin texture, expressive green eyes, soft natural lighting, cinematic sunset in the background, 8K resolution, ultra-detailed, photorealistic, focus on face, dynamic composition, rim lighting, mystical atmosphere"
+negative_prompt = "low quality, blurry, unrealistic, cartoonish, painting, drawing, 2D, abstract, deformed, oversaturated"
+guidance_scale = 8.5
+num_inference_steps = 80
 
 # Generate the base image (Text-to-Image)
 base_image = base_pipe(
@@ -40,7 +44,7 @@ base_image = base_pipe(
 ).images[0]
 
 # Save the base image
-base_image.save("output_base_image.png")
+base_image.save("output_base_SDXL1_0.png")
 
 # Refine the generated image by reintroducing it as input (Image-to-Image)
 refined_image = refiner_pipe(
@@ -53,6 +57,6 @@ refined_image = refiner_pipe(
 ).images[0]
 
 # Save the refined image
-refined_image.save("output_refined_image.png")
+refined_image.save("output_refined_SDXL1_0.png")
 
 print("Images generated and saved: 'output_base_image.png' and 'output_refined_image.png'")
